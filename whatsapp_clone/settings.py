0bad76a8f53fc57b545e2de543a999a5e7f21ca3
nareshb07 <k.nareshb0077@gmail.com>
2023-11-02 11:19:36 +0530

    main

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -101,7 +101,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("server1-redis.pfbfmq.ng.0001.aps1.cache.amazonaws.com", 6379)],
         },
     },
 }
